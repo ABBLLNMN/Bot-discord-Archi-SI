2ff827e44fea89b6eefe16c532b7b2789a80c1aa
Bilal <bilal.abbad@isep.fr>
2017-06-06 19:15:40 +0200

    dev forecast + changement weather

--- commands/weather.js
+++ commands/weather.js
@@ -2,8 +2,11 @@ const Command = require('./command')
 const config = require('../config')
 const http = require('http')
 const cityList = require('./city.list.json')
+
 const convertKelvinToCelsius = tempInKelvin => tempInKelvin - 273.15
+
 const getCityDetailsByName = name => cityList.find(city => city.name.toLowerCase() === name.toLowerCase())
+
 const getWeather = cityId => {
   return new Promise((resolve, reject) => {
     http.get(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&APPID=${config.weather.API_KEY}&lang=${config.weather.lang}`, function (res) {
@@ -21,7 +24,6 @@ const getWeather = cityId => {
       let rawData = ''
       res.on('data', data => {
         rawData += data
-        console.log(rawData)
       })
 
       res.on('end', () => {
@@ -36,6 +38,7 @@ const getWeather = cityId => {
     })
   })
 }
+
 module.exports = class Weather extends Command {
   static match (message) {
     console.log('checking  weather')
@@ -47,9 +50,7 @@ module.exports = class Weather extends Command {
 
     if (ville) {
       getWeather(ville.id).then(function (res) {
-        message.reply(`Voici la météo actuellement à ${ville.name} :
-    Temps : ${res.description}
-    Température : ${Math.round(res.temperature)}°C`)
+        message.reply(`Temps actuel à ${ville.name},${ville.country} :\nMétéo : ${res.description}\nTempérature : ${Math.round(res.temperature)}°C`)
       }).catch((error) => {
         console.log(error.message)
       })
