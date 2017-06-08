cbc29e23e7f380797c002c8b4c364e5987b1c01f
Lylath <nicolas-meneux@orange.fr>
2017-06-08 16:26:12 +0200

    Ajout du bot spotify dans les paramètres

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  token: '',
-  channel: '',
+  token: 'MzEzNjQ1OTcxNzA1OTU0MzE0.DAR4Ug.Z9cQH7Ghx3SiW7hp2_6nIhMgVOs',
+  channel: '306028002796371969',
   weather: {
     API_KEY: '',
     lang: ''
