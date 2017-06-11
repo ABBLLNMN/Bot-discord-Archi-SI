f22e176006fa44f29f7396d8314aef2461ae9364
Lylath <nicolas-meneux@orange.fr>
2017-06-11 14:40:33 +0200

    Petites améliorations

--- commands/translate.js
+++ commands/translate.js
@@ -1,8 +1,6 @@
 const Command = require('./command')
-const config = require('../config')
-
-var Trad = require('@google-cloud/translate')({
-  key: 'AIzaSyBngKK7gcXWaJVV3SmCZ40UmxFWCljOQx8'
+const Trad = require('@google-cloud/translate')({
+  key: 'AIzaSyCO0UTJvR2C3n6IiCioCiPJWJAuOEak18c'
 })
 
 module.exports = class Translate extends Command {
@@ -12,28 +10,23 @@ module.exports = class Translate extends Command {
   }
 
   static action (message) {
-
-    // detection de la langue
     const translate = Trad
     translate.detect(message.content)
       .then((results) => {
         let detections = results[0]
         detections = Array.isArray(detections) ? detections : [detections]
         detections.forEach((detection) => {
-          message.reply('Langue : ' + `${detection.language}`)
+          message.reply('Langue : ' + detection.language)
         })
       })
       .catch((err) => {
         console.log('ERROR', err)
       })
-    // traduction
     translate.translate(message.content, 'en').then((results) => {
       let translations = results[0]
       translations = Array.isArray(translations) ? translations : [translations]
-      message.reply('Traduction:')
-      console.log('Traduction:')
       translations.forEach((translation) => {
-        message.reply(`${message.content} => ${translation}`)
+        message.reply('Traduction : ' + (message.content + ' -> ' + translation)
       })
     })
     .catch((err) => {
