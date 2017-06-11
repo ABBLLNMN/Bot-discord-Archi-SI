baa1799a4c3bcbe2c06f0d9805aad8630371f261
Lylath <nicolas-meneux@orange.fr>
2017-06-11 17:26:08 +0200

    Ajout du bonus

--- commands/translate.js
+++ commands/translate.js
@@ -5,32 +5,40 @@ const Trad = require('@google-cloud/translate')({
 
 module.exports = class Translate extends Command {
   static match (message) {
-    console.log('checking googleTrad')
-    return message.content.startsWith('!translate')
+    return message.content.startsWith('translate')
   }
 
   static action (message) {
-    const translate = Trad
-    translate.detect(message.content)
+    // detection de la langue
+    if (message.content.charAt(2) !== ' ' || message.content.charAt(1) === ' ') {
+      message.reply('Veuillez entrer une langue valide.')
+    } else {
+      const translate = Trad
+      var retour = message.content.substring(0)
+      translate.detect(retour)
       .then((results) => {
         let detections = results[0]
         detections = Array.isArray(detections) ? detections : [detections]
         detections.forEach((detection) => {
-          message.reply('Langue : ' + detection.language)
+          message.reply('Langue de départ : ' + detection.language)
         })
       })
       .catch((err) => {
         console.log('ERROR', err)
       })
-    translate.translate(message.content, 'en').then((results) => {
-      let translations = results[0]
-      translations = Array.isArray(translations) ? translations : [translations]
-      translations.forEach((translation) => {
-        message.reply('Traduction : ' + (message.content + ' -> ' + translation))
+      // traduction dans la langue choisie
+      var languedemandee = message.content.charAt(0) + message.content.charAt(1)
+      var atraduire = message.content.substring(3)
+      translate.translate(atraduire, languedemandee).then((results) => {
+        let translations = results[0]
+        translations = Array.isArray(translations) ? translations : [translations]
+        translations.forEach((translation) => {
+          message.reply('La traduction : ' + translation)
+        })
       })
-    })
     .catch((err) => {
-      console.log('ERROR', err)
+      message.reply('ERROR', err)
     })
+    }
   }
-}
+  }
