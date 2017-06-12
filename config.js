0067322ad65714069eb7d5250483c33a7e320c50
LaetitiaBouvier <laetitia.bouvier@isep.fr>
2017-06-13 00:12:04 +0200

    avec le bon token c'est mieux

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'MzEzNjQ2MzE3MzIwNjY3MTM4.DCBDRQ.YMeeGF1yvUCMjADUehbjbwPP-ek',
+  token: 'MzEzNjQ2MDQ5MzM0MDAxNjY0.C_tJhg.1XLYYRNQgvHNns1ygWeJU8f_JiQ',
   channel: '306028002796371969',
   weather: {
     API_KEY: '51c3d12667251b88a09cbdeb2ec49ef5',
